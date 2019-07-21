1f78019a71a76d06d1557fdfbae75f7b01771b14
1368334540 <1368334540@qq.com>
2019-07-21 22:27:17 +0800

    my scrapy

--- test/json_load.py
+++ test/json_load.py
@@ -1,5 +1,5 @@
 import json
-with open(r"C:\Users\mi\PycharmProjects\mySpider\music.txt","r") as f:
+with open(r"C:\Users\mi\PycharmProjects\mySpider\mySpider\music.txt","r") as f:
 
 
     data = json.load(f)    #里面必须是一个字典 [,,,,,,] 在后面手动添加一个"]"吧
